7098b4c8a5ac29bafcc65f32c8c7352a117344d6
Roman Havryliuk <gavru4r@gmail.com>
2021-12-24 21:28:54 +0200

    Update 02-video.js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -6,30 +6,29 @@ import throttle from 'lodash.throttle'
 
 // Инициализирую плеер в файле скрипта
 const iframe = document.querySelector('iframe');
-    const player = new Vimeo.Player(iframe);
+    const player = new Player(iframe);
 
-    player.on('play', function() {
-        console.log('played the video!');
-    });
+    // player.on('play', function() {
+    //     console.log('played the video!');
+    // });
 
-    player.getVideoTitle().then(function(title) {
-        console.log('title:', title);
-    });
+    // player.getVideoTitle().then(function(title) {
+    //     console.log('title:', title);
+    // });
 
 // отслеживаю событие обновления времени воспроизвидения (взято из документации)
-const onPlay = function (data) {
-    
+const onPlay = function (data) {  
 // Сохраняю время воспроизведения в локальное хранилище и преобразовываю в JSON.
     localStorage.setItem("videoplayer-current-time", JSON.stringify(data.seconds));
-  
-};
-// timeupdate.. время воспроизведения.
-player.on('timeupdate', onPlay);
+//   data.seconds время воспроизведения
+    console.log(data.seconds);
 
+
+};
+player.on('timeupdate', throttle(onPlay,1000));
+    
 // возвращаю из хранилища ключ со значением
 const playTime = localStorage.getItem("videoplayer-current-time");
-
-
 
 // метод setCurrentTime() для того чтобы возобновить воспроизведение с сохраненной позиции(документация)
 player.setCurrentTime(playTime).then(function(seconds) {
