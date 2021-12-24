718a6065bc3765e308e62760b6c26535c7b072c0
Roman Havryliuk <gavru4r@gmail.com>
2021-12-24 17:54:45 +0200

    finish task 2 hw-8

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,47 @@
+// привязываю библиотеку проигрователя к файлу JS
+import Player from '@vimeo/player';
 
+// вызов метод тротл  для контроля количества раз вызова функци
+import throttle from 'lodash.throttle'
+
+// Инициализирую плеер в файле скрипта
+const iframe = document.querySelector('iframe');
+    const player = new Vimeo.Player(iframe);
+
+    player.on('play', function() {
+        console.log('played the video!');
+    });
+
+    player.getVideoTitle().then(function(title) {
+        console.log('title:', title);
+    });
+
+// отслеживаю событие обновления времени воспроизвидения (взято из документации)
+const onPlay = function (data) {
+    
+// Сохраняю время воспроизведения в локальное хранилище и преобразовываю в JSON.
+    localStorage.setItem("videoplayer-current-time", JSON.stringify(data.seconds));
+  
+};
+// timeupdate.. время воспроизведения.
+player.on('timeupdate', onPlay);
+
+// возвращаю из хранилища ключ со значением
+const playTime = localStorage.getItem("videoplayer-current-time");
+
+
+
+// метод setCurrentTime() для того чтобы возобновить воспроизведение с сохраненной позиции(документация)
+player.setCurrentTime(playTime).then(function(seconds) {
+    // seconds = the actual time that the player seeked to
+}).catch(function(error) {
+    switch (error.name) {
+        case 'RangeError':
+           
+            break;
+
+        default:
+          
+            break;
+    }
+});
